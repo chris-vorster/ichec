110f6937e1bfb2087dde402cf5bf84b06658257d
Chris Vorster <personal@chrisvorster.co.za>
2022-08-04 14:47:46 +0100

    Test ICHEC

---
+++ utils/pytorch_GpuQ_SLURM.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH -p GpuQ
+#SBATCH -N 1
+#SBATCH -t 00:30:00
+#SBATCH -A DCU01
+
+cd $SLURM_SUBMIT_DIR
+
+module load cuda/11.2
+module load conda/2
+
+source activate /ichec/work/dcu01/cvorster/py37
+
+HOME_DIR = '/ichec/home/users/cvorster/yolov7'
+WORK_DIR = '/ichec/work/dcu01/cvorster'
+
+echo "This is the GpuQ run."
+time python train.py --batch 16 --cfg '$HOME_DIR/cfg/training/yolov7.yaml' --epochs 2 --data '$WORK_DIR/data.yaml' --weights '$HOME_DIR/yolov7.pt' --device 0 --project '$WORK_DIR/test1'
