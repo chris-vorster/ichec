2dd5e64a616345e51b241aa26226c545898d1a73
Chris Vorster <personal@chrisvorster.co.za>
2022-08-05 10:53:58 +0100

    Changed to DevQ

--- utils/pytorch_multiGPU_yolov7_GpuQ_SLURM_detect.sh
+++ utils/pytorch_multiGPU_yolov7_GpuQ_SLURM_detect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH -p GpuQ
+#SBATCH -p DevQ
 #SBATCH -N 1
-#SBATCH -t 0:10:00
+#SBATCH -t 0:20:00
 #SBATCH -A DCU01
 
 cd $SLURM_SUBMIT_DIR
@@ -14,6 +14,6 @@ source activate /ichec/work/dcu01/cvorster/py37
 HOME_DIR='/ichec/home/users/cvorster/yolov7'
 WORK_DIR='/ichec/work/dcu01/cvorster'
 
-echo "This is the GpuQ detect run using https://github.com/WongKinYiu/yolov7"
+echo "This is the DevQ detect run using https://github.com/WongKinYiu/yolov7"
 cd $HOME_DIR
 time python detect.py --weights runs/train/yolov7-ipcam/weights/best.pt --conf 0.1 --source "$WORK_DIR/test/images"
